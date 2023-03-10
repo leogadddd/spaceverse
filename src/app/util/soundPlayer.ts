c58ca4ec8fb53009fdf4718be59830c643f00340
LeoooooGadil <leogadil04@gmail.com>
2023-03-11 01:07:46 +0800

    fix somethin

--- src/app/util/soundPlayer.ts
+++ src/app/util/soundPlayer.ts
@@ -5,7 +5,7 @@ export const playSound = (sound: HTMLAudioElement) => {
 
 	const Store: ISystemSettingsItemState | undefined = store.getState().systemSettings.systemSettings.find(setting => setting.id === "immediateToolbarMuteButton")
 
-	if(Store!.value)
+	if(Store && Store?.value === true)
 		return
 
 	if(sound.paused) {
